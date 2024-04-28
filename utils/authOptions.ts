bbfc105cff2e238ad5913d40f5b2696a4868b546
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-04-28 16:35:39 +0100

    Create authOptions config with presets.

---
+++ utils/authOptions.ts
@@ -0,0 +1,17 @@
+import Google from 'next-auth/providers/google';
+
+export const authOptions = {
+    providers: [
+        Google({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET,
+            authorization: {
+                params: {
+                    prompt: 'consent',
+                    access_type: 'offline',
+                    response_type: 'code',
+                },
+            },
+        }),
+    ],
+};
