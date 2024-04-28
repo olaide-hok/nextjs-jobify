31de1cad117b78ed9eacad546723e8cc02e59188
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-04-28 18:35:48 +0100

    Update client-id and client-secret env variable names

--- utils/authOptions.ts
+++ utils/authOptions.ts
@@ -3,8 +3,8 @@ import Google from 'next-auth/providers/google';
 export const authOptions = {
     providers: [
         Google({
-            clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_SECRET,
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
             authorization: {
                 params: {
                     prompt: 'consent',
