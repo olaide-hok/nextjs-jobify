6643d40065072ebfadef56ec22f8760bd2eaacc0
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-04-28 18:34:36 +0100

    Add configuration for images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        dangerouslyAllowSVG: true,
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                pathname: '**'
+            },
+            {
+                protocol: 'https',
+                hostname: 'source.boringavatars.com',
+                pathname: '**'
+            }
+        ]
+    }
+};
 
 export default nextConfig;
