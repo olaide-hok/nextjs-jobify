f898e922d36c007b06610ccda33af5fcb71f72c2
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-05-03 10:38:07 +0100

    Update authenticateAndRedirect Function

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,25 +1,19 @@
 'use server';
 
 import prisma from './db';
-// import {auth} from '@clerk/nextjs';
+import {auth} from '@/auth';
 import {JobType, CreateAndEditJobType, createAndEditJobSchema} from './types';
 import {redirect} from 'next/navigation';
 import {Prisma} from '@prisma/client';
 import dayjs from 'dayjs';
-import {auth} from '@/auth';
 
 async function authenticateAndRedirect(): Promise<string> {
-    try {
-        const session = await auth();
-        if (!session || !session?.user?.id) {
-            redirect('/');
-        }
-        const userId = session?.user?.id;
-        return userId;
-    } catch (error) {
-        console.error(error);
-        return 'Something went wrong!';
+    const session = await auth();
+    const userId = session?.user?.id;
+    if (!userId) {
+        redirect('/');
     }
+    return userId;
 }
 
 export async function createJobAction(
