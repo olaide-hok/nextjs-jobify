3a056e8efeff443bc809a125d107d80fbaadd8cc
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-05-03 18:22:03 +0100

    Setup JobsList component

---
+++ components/JobsList.tsx
@@ -0,0 +1,44 @@
+'use client';
+import JobCard from './JobCard';
+import {useSearchParams} from 'next/navigation';
+import {getAllJobsAction} from '@/utils/actions';
+import {useQuery} from '@tanstack/react-query';
+import {useSession} from 'next-auth/react';
+
+function JobsList() {
+    const searchParams = useSearchParams();
+    const {data: dataa} = useSession();
+    const userId = dataa?.user?.id! || '';
+
+    const search = searchParams.get('search') || '';
+    const jobStatus = searchParams.get('jobStatus') || 'all';
+
+    const pageNumber = Number(searchParams.get('page')) || 1;
+
+    const {data, isPending} = useQuery({
+        queryKey: ['jobs', search ?? '', jobStatus, pageNumber],
+        queryFn: () =>
+            getAllJobsAction({
+                search,
+                jobStatus,
+                page: pageNumber,
+                userId,
+            }),
+    });
+    const jobs = data?.jobs || [];
+
+    if (isPending) return <h2 className="text-xl">Please Wait...</h2>;
+
+    if (jobs.length < 1) return <h2 className="text-xl">No Jobs Found...</h2>;
+    return (
+        <>
+            {/*button container  */}
+            <div className="grid md:grid-cols-2  gap-8">
+                {jobs.map((job) => {
+                    return <JobCard key={job.id} job={job} />;
+                })}
+            </div>
+        </>
+    );
+}
+export default JobsList;
