ddffd01c028d1c45bd89a4eac6781918d0355985
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-05-06 10:58:00 +0100

    Redefine middleware configurations

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,16 @@
-export {auth as middleware} from './auth';
+import {auth} from '@/auth';
+import {NextResponse} from 'next/server';
 
 // Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+// Todo: match /api/auth/signin routes
 export const config = {
-    matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+    matcher: ['/add-job', '/stats', '/jobs/:path*'],
 };
+
+export default auth((req) => {
+    if (!req.auth) {
+        return NextResponse.redirect(new URL('/', req.url));
+    }
+});
 
 export const runtime = 'experimental-edge';
